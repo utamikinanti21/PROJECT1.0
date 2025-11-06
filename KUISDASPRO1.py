94b628f9464d078b177a8b18021e978ed6278401
utamikinanti21 <utamikinanti21@student.upi.edu>
2025-11-06 22:55:24 +0700

    menambahkan lama pengerjaan di kuisdaspro1

--- KUISDASPRO1.py
+++ KUISDASPRO1.py
@@ -9,8 +9,10 @@
 a = int(input("Masukkan panjang rumah : "))
 b = int(input("Masukkan tinggi rumah : "))
 c = int(input("Masukkan lebar rumah : "))
+f = a * b * c * 2 
 
 d = 2 * a * b + 2 * c * b 
 e = 450000 * d 
 
 print("Biaya pembuatan rumahmu adalah Rp.", e)
+print(f"Lama waktu yang dibutuhkan untuk membangun rumahmu adalah {f} hari")
